4e03896a95d1587d5c95b9334283cc301e47be43
Hyper <james_peter_wong@hotmail.com>
2017-08-20 16:47:46 +1200

    Allow colour change of background in InfoWindows

--- app/src/main/java/com/example/earth/fuelfriend/CustomInfoWindowAdapter.java
+++ app/src/main/java/com/example/earth/fuelfriend/CustomInfoWindowAdapter.java
@@ -1,15 +1,19 @@
 package com.example.earth.fuelfriend;
 
 import android.app.Activity;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
 
 import static com.example.earth.fuelfriend.Constants.TRANSPORT_CAR;
-import static com.example.earth.fuelfriend.GeneralHelper.getTransportIcon;
+import static com.example.earth.fuelfriend.GeneralHelper.getTransportColor;
 
 /**
  * Created by EARTH on 6/08/2017.
@@ -34,6 +38,7 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter{
 
         View view = context.getLayoutInflater().inflate(R.layout.custom_infowindow, null);
 
+        RelativeLayout rlBackground = (RelativeLayout) view.findViewById(R.id.rl_info_background);
         ImageView ivTransport = (ImageView) view.findViewById(R.id.iv_transport);
         TextView tvTitle = (TextView) view.findViewById(R.id.tv_title);
         TextView tvSubTitle = (TextView) view.findViewById(R.id.tv_subtitle);
@@ -42,11 +47,24 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter{
         String transport = marker.getTitle();
         transport = transport.substring(transport.indexOf("|") + 1, transport.length()); // Anchoring the Transport method onto the marker title. Such a hack..
         String savingStatus = (transport.equals(TRANSPORT_CAR)) ? "Used" : "Saved";
-        
-        ivTransport.setImageResource(getTransportIcon(transport));
+
+        Drawable circle = view.getResources().getDrawable(R.drawable.circle);
+        circle.setColorFilter(view.getResources().getColor(R.color.colorBikeLine), PorterDuff.Mode.MULTIPLY);
+
+        GradientDrawable bgDrawable = (GradientDrawable) rlBackground.getBackground();
+        bgDrawable.setColor(getTransportColor(transport, context));
+
+        //ivTransport.setImageResource(getTransportIcon(transport));
         tvTitle.setText(marker.getTitle().substring(0, marker.getTitle().indexOf("|"))); // Removing the transport method before displaying title of marker
         tvSubTitle.setText(marker.getSnippet());
         tvSavingStatus.setText(savingStatus);
+        //Changes the colour
+        //tvSavingStatus.setTextColor(savingStatusColour(savingStatus, view));
         return view;
     }
+
+    private int savingStatusColour(String status, View view) {
+        return status.equals("Used") ? view.getResources().getColor(R.color.colorCarLine) : view.getResources().getColor(R.color.colorBikeLine);
+    }
+
 }
