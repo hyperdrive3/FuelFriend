703f897cd9b06e6ec3fce618ab9a138d5bb50e7a
Hyper <james_peter_wong@hotmail.com>
2017-09-04 17:50:30 +1200

    Remove description text.

--- app/src/main/java/com/example/earth/fuelfriend/StatisticsFragment.java
+++ app/src/main/java/com/example/earth/fuelfriend/StatisticsFragment.java
@@ -2,11 +2,17 @@ package com.example.earth.fuelfriend;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerTabStrip;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+
+import java.util.ArrayList;
 
 /**
  * Created by EARTH on 30/08/2017.
@@ -20,23 +26,58 @@ public class StatisticsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     // Inflate the view for the fragment based on layout XML
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.stats_swipe_view, container, false);
+        View view = inflater.inflate(R.layout.statistics_home, container, false);
 
-        PagerTabStrip pagerTabStrip = (PagerTabStrip) view.findViewById(R.id.pager_header_stats);
-        pagerTabStrip.setDrawFullUnderline(true);
-        pagerTabStrip.setTabIndicatorColor(getResources().getColor(R.color.colorWalkLine));
+        LinearLayout summary = (LinearLayout) view.findViewById(R.id.stats_car);
 
-        ViewPager vpPager = (ViewPager) view.findViewById(R.id.vpStats);
-        mStatisticsAdapter = new StatisticsAdapter(getFragmentManager());
-        vpPager.setAdapter(mStatisticsAdapter);
+        summary.setClickable(true);
+        summary.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View arg0) {
+                // TODO Auto-generated method stub
 
+                return false;
+            }
+        });
+
+        PieChart pieChart = (PieChart) view.findViewById(R.id.pie_chart);
+
+
+        final int[] MY_COLORS = {getActivity().getResources().getColor(R.color.colorCarLine), getActivity().getResources().getColor(R.color.colorWalkLine), getActivity().getResources().getColor(R.color.colorBikeLine)};
+        ArrayList<Integer> colors = new ArrayList<>();
+
+        for (int c : MY_COLORS) colors.add(c);
+
+
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("Car");
+        labels.add("Walk");
+        labels.add("Bike");
+        PieDataSet dataset = new PieDataSet(entries, "Test");
+        dataset.setColors(colors);
+        dataset.setDrawValues(false);
+        PieData data = new PieData(labels, dataset);
+
+        pieChart.setData(data);
+        pieChart.setCenterText("16L ");
+        pieChart.highlightValue(1, 0);
+        pieChart.setDescription("");
+        pieChart.setTouchEnabled(false);
+        pieChart.highlightValues(null);
+
+        pieChart.setDrawSliceText(false);
+        pieChart.getLegend().setEnabled(false);
         return view;
     }
 
